9610ff78a68b3a3f8610878c74db97782f4be960
lohith <lohith@Lohith>
2022-05-25 08:43:43 +0530

    this project is for btm morning batch

---
+++ src/seleniumfour/SeleniumWebdriver.java
@@ -0,0 +1,59 @@
+package seleniumfour;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumWebdriver {
+	static {
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		WebDriver driver = new ChromeDriver();  driver.
+//		driver.findElement(null);//1   
+//		driver.findElements(null);//2
+//		driver.get(null);//3
+//		driver.close();//4
+//		driver.getCurrentUrl();//5
+//		driver.getPageSource();//6
+//		driver.getTitle();//7
+//		driver.getWindowHandle();//8
+//		driver.getWindowHandles();//9
+//		driver.manage();//10
+//		driver.navigate();//11
+//		driver.switchTo();//12
+//		driver.quit();//13
+//		 WebElement we;              
+//		 we.clear();//1
+//		 we.click();//2
+//		 we.findElement(null);//3
+//		 we.findElements(null);//4)
+//	     we.getAccessibleName();//5
+//	     we.getAriaRole();//6
+//	     we.getAttribute(null);//7t
+//	     we.getDomProperty(null);//8
+//	     we.getLocation();//9
+//	     we.getRect();//10
+//	     we.getShadowRoot();//11
+//	     we.getSize();//12
+//	     we.getTagName();//13
+//	     we.getText();//14
+//	     we.isDisplayed();//15
+//	     we.isEnabled();//16
+//	     we.isSelected();//17
+//	     we.sendKeys(args);//18
+//	     we.submit();//19
+//	     we.getDomAttribute(null);//20
+//	     we.getCssValue(null);//21
+//	     
+		 
+	
+	
+		
+		
+
+	}
+
+}
